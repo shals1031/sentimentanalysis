a26f7399d3e60ffac1fa3749006b8dd67cb3275d
Shaleen Pandiya <88051291+shals1031@users.noreply.github.com>
2024-10-02 14:43:32 +0530

    Add files via upload

---
+++ SentimentAnalysis/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure;
+using System;
+using Azure.AI.TextAnalytics;
+using System.Collections.Generic;
+using SentimentAnalysis;
+
+namespace Example
+{
+    class Program
+    {
+        static string languageKey = "e61d1f94050a4679ab56c6a55d8ecf08";
+        static string languageEndpoint = "https://mmsentimentanalysis.cognitiveservices.azure.com/";
+
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential(languageKey);
+        private static readonly Uri endpoint = new Uri(languageEndpoint);
+
+        
+        static void Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            SentimentCalculation.SentimentAnalysisWithOpinionMiningExample(client);
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+}
